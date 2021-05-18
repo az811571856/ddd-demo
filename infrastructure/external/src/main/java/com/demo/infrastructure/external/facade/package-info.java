7747562e3fceb15d4e16c49c57afa442f0cdabb3
az811571856 <811571856@qq.com>
2021-05-18 17:45:32 +0800

    refactor package

--- infrastructure/external/src/main/java/com/demo/infrastructure/external/facade/package-info.java
+++ infrastructure/external/src/main/java/com/demo/infrastructure/external/facade/package-info.java
@@ -1,1 +1,5 @@
 package com.demo.infrastructure.external.facade;
+
+//此包是对外部服务的防腐层
+//此包放XXFacade类，在Application层external层中间，Application直接调用的是facade屏蔽外部服务的变化
+//dto里放转换后的代码
--- infrastructure/external/src/main/java/com/demo/infrastructure/external/origin/package-info.java
+++ infrastructure/external/src/main/java/com/demo/infrastructure/external/origin/package-info.java
@@ -1,1 +1,6 @@
-package com.demo.infrastructure.external;
+package com.demo.infrastructure.external.origin;
+
+
+//放依赖的第三方服务代码
+//此包放Service接口
+//impl放实现代码
